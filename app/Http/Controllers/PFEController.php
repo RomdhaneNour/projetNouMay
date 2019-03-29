873b542037a377b398ad98bb0764f119df723293
Nour_Romdhane <Romdhane_Nour@outlook.fr>
2019-03-29 17:10:23 +0100

    tfadhal lala deba

---
+++ app/Http/Controllers/PFEController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PFE;
+use Illuminate\Http\Request;
+
+class PFEController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PFE  $pFE
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PFE $pFE)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PFE  $pFE
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PFE $pFE)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PFE  $pFE
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PFE $pFE)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PFE  $pFE
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PFE $pFE)
+    {
+        //
+    }
+}
---
+++ database/factories/PFEFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PFE::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
